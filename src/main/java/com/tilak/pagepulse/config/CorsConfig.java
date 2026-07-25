5d5f625fe396cbea1d9ce09832a01d2602a9c662
Tilak Kumar <kumartilakk29@gmail.com>
2026-07-25 11:37:08 +0530

    Use CorsFilter

--- src/main/java/com/tilak/pagepulse/config/CorsConfig.java
+++ src/main/java/com/tilak/pagepulse/config/CorsConfig.java
@@ -1,21 +1,31 @@
 package com.tilak.pagepulse.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    @Bean
+    public CorsFilter corsFilter() {
 
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "https://pagepulse-rho.vercel.app"
-                )
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(false);
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
